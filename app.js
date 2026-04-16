18f72b62ad6235c4d3cb77d362caf9bb2fd5dd43
tahsinulhoque <tahsinulhoquesiddiki@gmail.com>
2026-04-16 21:13:28 +0600

    app, server code upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use('/api/students', require('./routes/studentRoutes'));
+
+module.exports = app;
