4c34c93b1e9431c47a3232c746f2b871d4772ffb
tahsinulhoque <tahsinulhoquesiddiki@gmail.com>
2026-04-17 11:08:17 +0600

    change package

--- Backend/app.js
+++ Backend/app.js
@@ -8,6 +8,6 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-app.use('/api/students', require('./routes/studentRoutes'));
+app.use('/api/students', require('./routes/studentRoutes.js'));
 
 module.exports = app;
